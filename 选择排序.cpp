4b02445e447986cced4829167cecf7fd31724299
lmjsy <90953103+lmjsy@users.noreply.github.com>
2021-10-24 01:18:07 +0800

    Add files via upload

---
+++ 选择排序.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 8
+void select_sort(int a[],int n)//n为数组a的元素个数
+{
+    //进行N-1轮选择
+    for(int i=0; i<n-1; i++)
+    {
+        int min_index = i; 
+        //找出第i小的数所在的位置
+        for(int j=i+1; j<n; j++)
+        {
+            if(a[j] < a[min_index])
+            {
+                min_index = j;
+            }
+        }
+        //将第i小的数，放在第i个位置；如果刚好，就不用交换
+        if( i != min_index)
+        {
+            int temp = a[i];
+            a[i] = a[min_index];
+            a[min_index] = temp;//如果不是，就进行交换 
+        }
+    }
+}
+int  main()
+{
+    int num[N] = {89, 38, 11, 78, 96, 44, 19, 25};
+    select_sort(num, N);
+    for(int i=0; i<N; i++)
+        printf("%d  ", num[i]);
+    printf("\n");
+    system("pause");
+    return 0;
+}
